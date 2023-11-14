1623b12edbd9cfdeca98f564047cca1e259dd157
simransin <simransingh9876rajgir@gmail.com>
2023-11-14 18:00:37 +0530

    all string  method

--- 6_String_methods.js
+++ 6_String_methods.js
@@ -9,3 +9,16 @@ firstname.trim(); // because of string is immutable sting will not trim the spac
 let newstring = firstname.trim();
 console.log(newstring);
 console.log(newstring.length);
+
+
+//toUpperCase(), it will change lowercase to uppercase
+console.log(firstname.toUpperCase());
+
+//toLowerCase(), uppercase to lowercase
+console.log(firstname.toLowerCase());
+
+// slice(), it will start from start index and end index
+console.log(firstname.slice(0,2));
+
+let newstring2 = firstname.slice(0,6);
+console.log(newstring2);
