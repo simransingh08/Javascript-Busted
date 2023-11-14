1770b6a56d6e667d5cbfca3c28150618ff157b78
simransin <simransingh9876rajgir@gmail.com>
2023-11-14 17:21:51 +0530

    Experiment with Var

--- var.js
+++ var.js
@@ -2,3 +2,10 @@ var firstname = "Simran";
 var lastname = 'Singh';
 
 console.log(firstname+" " +lastname);
+
+firstname = "Veer";
+
+console.log(firstname+" " +lastname);
+lastname = "Kunwer Bharti";
+
+console.log(firstname+" " +lastname);
