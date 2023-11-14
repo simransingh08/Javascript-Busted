138aea61802a55204704e2e37e42e599c7a8ac51
simransin <simransingh9876rajgir@gmail.com>
2023-11-14 17:29:42 +0530

    Adding let keyword

--- let.js
+++ let.js
@@ -1,1 +1,10 @@
-let
+// let Keyword
+// I'm going to declare variable with let keyword
+
+let firstname = "Simran";
+console.log(firstname);
+
+//let firstname = "Singh"; // we can't re-declare the same variable with the let keyword
+
+//console.log(firstname);
+
