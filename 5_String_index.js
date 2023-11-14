2e412c610a95d18b31b3b9901af2b800616864d3
simransin <simransingh9876rajgir@gmail.com>
2023-11-14 17:41:41 +0530

    String Indexing

---
+++ 5_String_index.js
@@ -0,0 +1,18 @@
+// String Indexing
+
+let firstname = "Simran";
+
+// S i m r a n
+// 0 1 2 3 4 5
+
+console.log(firstname)
+
+console.log(firstname[3]);
+
+// String Length
+
+console.log(firstname.length);
+
+// Last index
+
+console.log(firstname[firstname.length-1]);
