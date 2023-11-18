b583902360535f1820a96a829d6b7d78d35f2c1d
simransin <simransingh9876rajgir@gmail.com>
2023-11-18 23:11:15 +0530

    Escape Character

---
+++ 8.2_Escape_Char.js
@@ -0,0 +1,6 @@
+// Using escape characters
+console.log("This is a string with a newline: \nNew line here.");
+console.log("This is a string with a tab: \tTabbed text here.");
+console.log("This is a string with a backslash: \\");
+console.log("This is a string with double quotes: \"Double quotes here.\"");
+console.log('This is a string with single quotes: \'Single quotes here.\'');
