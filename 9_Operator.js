0860ec4862b54cec18d42600675add4083f01180
simransin <simransingh9876rajgir@gmail.com>
2023-11-19 15:59:29 +0530

    Operator

---
+++ 9_Operator.js
@@ -0,0 +1,34 @@
+
+// Arithmetic Operators
+let a = 5;
+let b = 9;
+console.log("a =", a, "& b =", b);
+console.log("a + b =", a + b);
+console.log("a - b =", a - b);
+console.log("a * b =", a * b);
+console.log("a / b =", a / b);
+console.log("a % b =", a % b);
+console.log("a ** b =", a ** b);
+
+// unary Operator
+let c = 4;
+let d = 8;
+
+console.log("c = ", c, "& d =", d);
+c++;// post increment operator
+console.log("c = ", c);
+d--; // post decrement operator
+console.log("d = ", d);
+
+
+let e = 4;
+let f = 8;
+++e;// post increment operator
+console.log("e = ", e);
+--f; // post decrement operator
+console.log("f = ", f);
+
+
+
+
+
