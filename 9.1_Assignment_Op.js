b0a4f5e5628f7b25eee12a01efd90f4f6527cad0
simransin <simransingh9876rajgir@gmail.com>
2023-11-20 23:16:07 +0530

    Assignment Operator

---
+++ 9.1_Assignment_Op.js
@@ -0,0 +1,23 @@
+
+let a = 5;
+let b = 2;
+
+a += 4; // a = a+4
+console.log("a = ", a);
+
+a -= 4; // a = a-4
+console.log("a = ", a);
+
+a *= 4; // a = a*4
+console.log("a = ", a);
+
+a /= 4; // a = a/4
+console.log("a = ", a);
+
+a %= 4; // a = a%4
+console.log("a = ", a);
+
+e =5; 
+f =4;
+e **= 4; // e = e**4
+console.log("e = ", e);
