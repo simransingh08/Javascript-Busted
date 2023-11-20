2348d3b5b554cf9bfb2e5272bd1674df822e3fd5
simransin <simransingh9876rajgir@gmail.com>
2023-11-20 22:45:10 +0530

    Const

--- 4_const.js
+++ 4_const.js
@@ -1,4 +1,4 @@
 // declaring const keyword
-
+//when ever you just want any unchangelabe value in your program you can go with const
 const pi =3.14; // using const key for constant variable or mathematical uses
 console.log(pi); 
