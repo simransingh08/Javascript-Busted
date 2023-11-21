d99ac831b6cfe16314bed5ec7b3cb9b3d6c5ec5e
simransin <simransingh9876rajgir@gmail.com>
2023-11-21 08:04:01 +0530

    Logical Operator

---
+++ 9.3_Logical_op.js
@@ -0,0 +1,25 @@
+//Logical AND operator
+let a = true;
+let b = false;
+console.log(a && b); // false
+
+let a1 = 5;
+let b1 = 10;
+let c1 = 15;
+console.log(a < b && b < c); // true because both conditions are true
+
+//logical OR Operator
+let a2 = true;
+let b2 = false;
+console.log(a || b); // true
+
+let hour = 12;
+console.log(hour < 10 || hour > 14); // true because at least one condition is true
+
+//Logical NOT
+let a3 = true;
+console.log(!a); // false
+
+let sunny = true;
+console.log(!sunny); // false because !sunny negates the true value
+
