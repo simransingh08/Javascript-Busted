9de9c4e9d8060f5dd52cc65d3fc0209c4dc13817
simransin <simransingh9876rajgir@gmail.com>
2023-11-21 08:49:45 +0530

    Ternary Opertaor

---
+++ 9.4_Ternary_Op.js
@@ -0,0 +1,9 @@
+// syntax: condition?true ouput : false output
+
+let age = 26;
+let result = age>= 18 ? "Adult": "Not Adult";
+console.log(result);
+
+let age2 =16;
+let result2 = age2>=18 ? "Adult": "Not Adult";
+console.log(result2);
