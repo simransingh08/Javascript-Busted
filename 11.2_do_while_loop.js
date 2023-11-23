3157fbea1e85221a7f52bd9ac48f353a80adb245
simransin <simransingh9876rajgir@gmail.com>
2023-11-23 19:44:28 +0530

    do while loop

---
+++ 11.2_do_while_loop.js
@@ -0,0 +1,16 @@
+// do while loop
+
+let i = 20;
+do{
+    console.log("JS");
+    i++;
+}while (i <= 10);
+
+// example
+
+let j = 1;
+do{
+    console.log("j = ", j);
+    j++;
+
+}while( j <= 5);
