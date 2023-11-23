5e05ca3095e0f8f16b4d0356f4f439d6aae368cc
simransin <simransingh9876rajgir@gmail.com>
2023-11-23 19:29:02 +0530

    For Loop

---
+++ 11_forloop.js
@@ -0,0 +1,22 @@
+// for loop
+
+for(let i = 1; i<=5; i++)
+{
+    console.log("Javascript");
+}
+
+//Example
+
+for(j = 0; j<=10; j++)
+{
+    console.log("j = ",j+1)
+}
+
+// calculate sum of 1 to 5
+let sum = 0;
+for(let k = 1; k <= 5; k++)
+{
+    sum = sum + k;
+}
+console.log("Sum = ", sum);
+console.log("Loop has ended");
