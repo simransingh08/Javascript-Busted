04b329564840b427177192f217e5283d72e09564
simransin <simransingh9876rajgir@gmail.com>
2023-11-24 17:26:47 +0530

    Even Number example

---
+++ Programs/3_Prog.js
@@ -0,0 +1,15 @@
+// Print all even numbers from 0 to 100.
+
+
+for(let num=0; num<=100; num++)
+{
+    console.log("num =", num);
+}
+
+for( let num1=0; num1<=100; num1++){
+    if(num1%2 ===0)
+    {
+        console.log("num1", num1);
+    }
+    
+}
