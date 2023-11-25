6fc1950692bac54a474fa35e0b32b45e0f137ee5
simransin <simransingh9876rajgir@gmail.com>
2023-11-25 10:36:03 +0530

    Creating array

---
+++ 12_Array.js
@@ -0,0 +1,6 @@
+//Creating a array for student marks
+let marks = [93,82,75,64,39];
+console.log(marks);
+
+let names = ['spiderman', 'batman', 'hulk', 'shaktimaan', 'ironman'];
+console.log(names);
