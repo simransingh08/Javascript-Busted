58442b1f15ce46423f8a4b5caa4dd84c94c2c19c
simransin <simransingh9876rajgir@gmail.com>
2023-11-25 14:44:38 +0530

    Array Indexing

---
+++ 12.2_Array_index.js
@@ -0,0 +1,13 @@
+// array index
+
+let arr = [56,85,45,3,599,6,85,];
+console.log(arr);
+
+//print particular value with indexing
+console.log(arr[5]);
+
+// If you want to change index value
+arr[3]= 90;
+console.log(arr);
+console.log(arr[3])
+
