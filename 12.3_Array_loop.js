cc782070f4a3565877e771e7cbbaba7039044531
simransin <simransingh9876rajgir@gmail.com>
2023-11-25 15:02:34 +0530

    Loop in Array

---
+++ 12.3_Array_loop.js
@@ -0,0 +1,12 @@
+//Looping over arrays
+let names = ['spiderman', 'batman', 'hulk', 'shaktimaan', 'ironman'];
+for(let idx =0; idx<names.length; idx++)
+{
+    console.log(names[idx]);
+}
+
+// for of
+for (let name of names)
+{
+    console.log(names);
+}
