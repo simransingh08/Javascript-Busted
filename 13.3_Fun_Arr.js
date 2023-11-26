2dea93622ce2618800f537416fb8a78107f1275c
simransin <simransingh9876rajgir@gmail.com>
2023-11-26 19:52:19 +0530

    Arrow function in funtion

---
+++ 13.3_Fun_Arr.js
@@ -0,0 +1,19 @@
+// Array Function
+
+function sum(a, b){
+    return a+b;
+}
+
+//Modern js part
+const arrowSum = (a,b) => {
+    console.log(a+b);
+};
+
+// multiplication function
+function mul(a,b){
+    return a*b;
+}
+
+const arrowMul = (a,b) => {
+    console.log(a*b);
+}
