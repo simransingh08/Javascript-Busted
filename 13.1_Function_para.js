eb30439456fee24ca575e4badadceaf5544c44a7
simransin <simransingh9876rajgir@gmail.com>
2023-11-26 15:18:16 +0530

    Function parameter and argument

---
+++ 13.1_Function_para.js
@@ -0,0 +1,9 @@
+// Function parameter
+
+// mgs is a function parameter, when we defining function, that's time we call parameter
+function myFunction(mgs){
+    console.log(mgs);
+}
+
+//Learning JS is a argument, when we calls the function then we need to argument for print the output
+myFunction("Learning JS");
