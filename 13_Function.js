12c6eb20fe4a3a9760509c34a1771741682ae569
simransin <simransingh9876rajgir@gmail.com>
2023-11-26 14:07:01 +0530

    Function

---
+++ 13_Function.js
@@ -0,0 +1,10 @@
+// Functions:- Block of code that performs a specific task, can be invoked(call) whenever needed.
+
+// defining function
+function myfunction(){
+    console.log("Welcome to Javascript Busted");
+    console.log('We are learning JS');
+}
+
+//calling function
+myfunction();
