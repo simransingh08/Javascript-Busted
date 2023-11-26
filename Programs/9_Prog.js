42c5925620cc9eddb1f03f7f864157f0c201ae75
simransin <simransingh9876rajgir@gmail.com>
2023-11-26 20:23:02 +0530

    Count vowel with arrow function program

--- Programs/9_Prog.js
+++ Programs/9_Prog.js
@@ -9,5 +9,18 @@ function countVowel (str){
         { count ++; }
     }
 
-    console.log(count);
+    //console.log(count);
+    return count;
+}
+
+// with arrow function
+const countVow = (str) => {
+    let count = 0;
+    for (const char of str){
+        if ( char === "a" || char === "e" || char === "i" || char === "o" || char === "u")
+        { count ++; }
+    }
+
+    //console.log(count);
+    return count;
 }
