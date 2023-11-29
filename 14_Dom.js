e4a14c9f6d7abed4692f419d2cd66052fc9de87c
simransin <simransingh9876rajgir@gmail.com>
2023-11-29 15:52:34 +0530

    Query Selector

---
+++ 14_Dom.js
@@ -0,0 +1,17 @@
+// DOM (Document Object Model)
+console.log("Hi, I'm going to learn the most popular concept DOM"); 
+
+// DOM Manipulation
+
+// 1. selecting with id, document.getElementById("myId")
+// let header = document.getElementById("heading");
+// console.dir(header);
+
+// 2. selecting with class, document.getElementByClassName("myClass")
+let headings = document.getElementsByClassName("heading");
+console.dir(headings);
+console.log(headings);
+
+// 3. selecting with tag, document.getElementByTagName("p")
+let paras = document.getElementsByTagName("p");
+console.dir(paras);
---
+++ 15_Dom_Query_selector.js
@@ -0,0 +1,7 @@
+// this is for only first element
+// let elements = document.querySelector('p');
+// console.log(elements);
+
+// this is for whole element
+let allEle = document.querySelectorAll(".heading");
+console.dir(allEle);
