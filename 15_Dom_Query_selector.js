9e140a240a83c9098abe11d5ca9ffd5600a5ab70
simransin <simransingh9876rajgir@gmail.com>
2023-12-04 20:57:27 +0530

    Modifying

--- 15_Dom_Query_selector.js
+++ 15_Dom_Query_selector.js
@@ -1,7 +1,3 @@
-// this is for only first element
-// let elements = document.querySelector('#myID');
-// console.log(elements);
-
 // this is for whole element
 // let allEle = document.querySelectorAll(".heading");
 // console.dir(allEle);
