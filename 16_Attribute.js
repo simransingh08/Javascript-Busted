11c2df4b99f20fd45f5ecd443c473e3577e476e8
simransin <simransingh9876rajgir@gmail.com>
2023-12-07 12:51:22 +0530

    Get Attribute

---
+++ 16_Attribute.js
@@ -0,0 +1,17 @@
+
+// how to get attribute
+let div = document.querySelector("div")
+console.log(div);
+
+// getting the attribute from selector
+let id = div.getAttribute("id");
+console.log(id);
+let name = div.getAttribute("name");
+console.log(id);
+
+let para = document.querySelector("p");
+console.log(para.getAttribute('class'));
+
+
+// how to set attribute
+
