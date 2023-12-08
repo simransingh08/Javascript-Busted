f2b62416684558641e373d39f23a0f1e0c3eeb35
simransin <simransingh9876rajgir@gmail.com>
2023-12-08 23:28:30 +0530

    Insert things

--- 17_Insert.js
+++ 17_Insert.js
@@ -1,3 +1,20 @@
 let newBtn = document.createElement("button");
 newBtn.innerText = "Click Me!";
 console.log(newBtn);
+
+//if we want button on top then use prepend
+// let div = document.querySelector("div");
+// div.prepend(newBtn);
+
+//if we want button in bottom then use append
+// let div = document.querySelector("div");
+// div.append(newBtn);
+
+// if we want just before the content then use before
+// let div = document.querySelector("div");
+// div.before(newBtn);
+
+// if we want just before the content then use before
+let div = document.querySelector("div");
+div.after(newBtn);
+
