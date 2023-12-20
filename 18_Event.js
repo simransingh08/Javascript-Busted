267c66948fcd42543a5adf679f0369a750d4d061
simransin <simransingh9876rajgir@gmail.com>
2023-12-21 00:07:45 +0530

    Event Listener Done

--- 18_Event.js
+++ 18_Event.js
@@ -8,16 +8,41 @@ let btn = document.querySelector("#Btn1");
 // };
 
 // events object
-btn.onclick = (e) => {
-    console.log("Event Object");
-    console.log(e);
-    console.log(e.type);
-    console.log(e.target);
-    console.log( e.clientX, e.clientY);
+// btn.onclick = (e) => {
+//     console.log("Event Object");
+//     console.log(e);
+//     console.log(e.type);
+//     console.log(e.target);
+//     console.log( e.clientX, e.clientY);
     
-}
+// }
 
-let box = document.querySelector('div');
-box.onmouseover = () =>{
-    console.log("you are inside div")
-};
+// let box = document.querySelector('div');
+// box.onmouseover = () =>{
+//     console.log("you are inside div")
+// };
+
+// event listener
+btn.addEventListener("click", () => {
+    console.log("button was clicked - handler1");
+});
+
+btn.addEventListener("click", () => {
+    console.log("button was clicked - handler2");
+});
+
+//remove this button
+
+const handler3 = () => {
+    console.log("button was clicked - handler3");
+}
+btn.addEventListener("click", () => {  
+});
+
+btn.removeEventListener("click", handler3);
+
+btn.addEventListener("click", () => {
+    console.log("button was clicked - handler4");
+});
+
+
