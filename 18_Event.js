c5b96c006751d921d2f9f7be7d092d002a1459c3
simransin <simransingh9876rajgir@gmail.com>
2023-12-20 23:53:48 +0530

    Event Object done

--- 18_Event.js
+++ 18_Event.js
@@ -1,7 +1,23 @@
+// events handler
 let btn = document.querySelector("#Btn1");
-btn.onclick = () =>  {
-    console.log("Btn1 was clicked");
-    let a = 25;
-    a++;
-    console.log(a); 
+// btn.onclick = () =>  {
+//     console.log("Btn1 was clicked");
+//     let a = 25;
+//     a++;
+//     console.log(a); 
+// };
+
+// events object
+btn.onclick = (e) => {
+    console.log("Event Object");
+    console.log(e);
+    console.log(e.type);
+    console.log(e.target);
+    console.log( e.clientX, e.clientY);
+    
+}
+
+let box = document.querySelector('div');
+box.onmouseover = () =>{
+    console.log("you are inside div")
 };
