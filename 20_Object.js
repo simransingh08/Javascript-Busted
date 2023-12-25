902e7ff1c50d00d124d306e2d2f4ee8d16fb6b86
simransin <simransingh9876rajgir@gmail.com>
2023-12-25 22:35:57 +0530

    Object in js

---
+++ 20_Object.js
@@ -0,0 +1,26 @@
+// Object is a reference type like array
+
+//creating an object
+// const person = {
+//     name: "Veer",
+//     age: 23,
+//     place: "Noida"
+// }
+// // console.log(person);
+
+// // how to access data from objects
+// console.log(person.name);
+// console.log(person.place); 
+
+// we can create array within object also
+const simran = {
+    name: "Simran",
+    age: 23,
+    hobbies: ["cooking", "Teaching", "Writing", "Cleaning"]
+}
+// // console.log(simran);
+// console.log(simran.hobbies);
+
+// how to add key pair value
+simran.gender = "female";
+console.log(simran);
