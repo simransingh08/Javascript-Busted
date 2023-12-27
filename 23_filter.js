46b2dadd0a57583498cb4dd81178f7f970c1c1c9
simransin <simransingh9876rajgir@gmail.com>
2023-12-27 17:35:46 +0530

    filter

---
+++ 23_filter.js
@@ -0,0 +1,25 @@
+const arr  = [5,1,3,2,6];
+
+//filter odd values
+
+function isOdd(x){
+    return x%2;
+}
+
+const output = arr.filter(isOdd);
+console.log(output);
+
+// filter Even Values
+function isEven(x){
+    return x%2 === 0;
+}
+
+const outputeve = arr.filter(isEven);
+console.log(outputeve);
+
+// filter Even Values
+//we can pass function in higherOrder function style also
+const greater = arr.filter(function isgreaterThan4(x){
+    return x>4;
+});
+console.log(greater);
