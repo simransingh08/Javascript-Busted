e608cd279723581dc79e4ada1bb05195ca89f3d4
simransin <simransingh9876rajgir@gmail.com>
2023-12-27 17:40:31 +0530

    filter using arrow function

--- 23_filter.js
+++ 23_filter.js
@@ -23,3 +23,8 @@ const greater = arr.filter(function isgreaterThan4(x){
     return x>4;
 });
 console.log(greater);
+
+//filter less than
+// we can use arrow function for filtring
+const less = arr.filter((x) => x<5);
+console.log(less);
